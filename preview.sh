4763dcf0c7becae367170acd6a6e7ba74b1cc6e4
Garrett D'Amore <garrett@damore.org>
2017-11-07 13:46:21 -0800

    New publication workflow, zerotier RFC, and updates to old RFCs.

---
+++ preview.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+# Copyright 2017 Garrett D'Amore <garrett@damore.org>
+# Copyright 2017 Capitar IT Group BV <info@capitar.com>
+# This software is supplied under the terms of the MIT License, a
+# copy of which should be located in the distribution where this
+# file was obtained (LICENSE.txt).  A copy of the license may also be
+# found online at https://opensource.org/licenses/MIT.
+#
+
+# This script is used to preview in HTML or man format, the documentation.
+# I use it during development, YMMV.  It is probably completely useless
+# on Windows.
+
+case $(uname -s) in
+Darwin)
+	OPEN=open
+	MAN=man
+	;;
+Linux)
+	OPEN=xdg-open
+	MAN=man
+	;;
+*)
+	echo "No idea how to preview on this system."
+	exit 2
+esac
+
+if [[ -n "$DISPLAY" ]]
+then
+	style=html
+else
+	style=man
+fi
+
+while getopts hmc arg
+do
+	case "${arg}" in
+	h)	style=html;;
+	m)	style=man;;
+	c)	cleanup=yes;;
+	?)	echo "Usage: $0 [-h|-m] <files...>"; exit 1 ;;
+	esac
+done
+shift $(( $OPTIND - 1 ))
+
+open_man=${MAN}
+open_html=${OPEN}
+suffix_html=".html"
+suffix_man=".man"
+backend_html="html5"
+backend_man="manpage"
+diagram_html="-r asciidoctor-diagram"
+diagram_man=""
+name=nng
+
+if [ -n "${cleanup}" ]
+then
+	tempdir=$(mktemp -d)
+	clean() {
+		sleep 1
+		rm -rf ${tempdir}
+	}
+	trap clean 0
+	mkdir -p ${tempdir}
+else
+	tempdir=/tmp/${LOGNAME}.${name}.preview
+	mkdir -p ${tempdir}
+fi
+
+eval backend=\$backend_${style}
+eval suffix=\$suffix_${style}
+eval view=\$open_${style}
+eval diagram=\$diagram_${style}
+
+
+for input in "$@"; do
+	base=$(basename $input)
+	base=${base%.adoc}
+	output=${tempdir}/${base}${suffix}
+	asciidoctor -D ${tempdir} -a imagesoutdir=${tempdir} -b ${backend} ${diagram} $input
+	$view ${output}
+done
---
+++ publish.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#
+# Copyright 2017 Garrett D'Amore <garrett@damore.org>
+# Copyright 2017 Capitar IT Group BV <info@capitar.com>
+# This software is supplied under the terms of the MIT License, a
+# copy of which should be located in the distribution where this
+# file was obtained (LICENSE.txt).  A copy of the license may also be
+# found online at https://opensource.org/licenses/MIT.
+#
+# 
+# This program attempts to publish updated documentation to nanomsg
+# gh-pages branch, in the rfcs directory.
+# 
+# This script requires asciidoctor, pygments, git, asciidoctor-diagram,
+# ditaa, packetdiag (from nwdiag) and a UNIX shell.
+# 
+
+tmpdir=$(mktemp -d)
+srcdir=$(dirname $0)
+dstdir=${tmpdir}/pages
+dstrfcs=${dstdir}/rfcs
+cd ${srcdir}
+name=rfcs
+
+giturl="${GITURL:-git@github.com:nanomsg/nanomsg}"
+
+cleanup() {
+	echo "DELETING ${tmpdir}"
+	rm -rf ${tmpdir}
+}
+
+mkdir -p ${tmpdir}
+
+trap cleanup 0
+
+echo git clone ${giturl} ${dstdir} || exit 1
+git clone ${giturl} ${dstdir} || exit 1
+
+(cd ${dstdir}; git checkout gh-pages)
+
+[ -d ${dstrfcs} ] || mkdir -p ${dstrfcs}
+
+dirty=
+for input in $(find . -name '*.adoc'); do
+	adoc=${input#./}
+	html=${adoc%.adoc}.html
+	output=${dstrfcs}/${html}
+
+	status=$(git status -s $input )
+	when=$(git log -n1 --format='%ad' '--date=format-local:%s' $input )
+	cat <<EOF > ${output}
+---
+layout: default
+---
+EOF
+
+	if [ -n "$when" ]
+	then
+		epoch="SOURCE_DATE_EPOCH=${when}"
+	else
+		epoch=
+		dirty=yes
+	fi
+	if [ -n "$status" ]
+	then
+		echo "File $adoc is not checked in!"
+		dirty=yes
+	fi
+
+
+	env ${epoch} asciidoctor \
+		-askip-front-matter \
+		-bhtml5 \
+		-r asciidoctor-diagram \
+		-D ${dstrfcs} \
+		${adoc} >> ${output}
+	chmod 0644 ${output}
+
+	if [ $? -ne 0 ]
+	then
+		echo "Failed to process $adoc !"
+		fails=yes
+	fi
+		
+
+done
+
+if [ -n "$dirty" ]
+then
+	echo "Repository has uncommited documentation.  Aborting."
+	exit 1
+fi
+
+if [ -n "$fails" ]
+then
+	echo "Failures formatting documentation. Aborting."
+	exit 1
+fi
+
+(cd ${dstrfcs}; pwd; echo git add *.html *.png; git add *.html *.png; git commit -m "rfc updates"; git push origin gh-pages)
